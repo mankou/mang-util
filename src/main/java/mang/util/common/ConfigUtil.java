ed3df3f50c925abacdc2518722fab195b5607d80
mang <ningma003@gmail.com>
2017-12-21 16:50:47 +0800

    * configutil 取配置文件路径是否输出日志

--- src/main/java/mang/util/common/ConfigUtil.java
+++ src/main/java/mang/util/common/ConfigUtil.java
@@ -29,16 +29,45 @@ public class ConfigUtil {
 	 * @return String 路径
 	 */
 	public static String getConfigPathFromDefault(String filePath, String defaultPath) {
+		return getConfigPathFromDefault(filePath, defaultPath, true);
+	}
+
+	/**
+	 * 优先从工作空间取 如果找不到就使用默认的路径 目前用于加载spring的配置文件.
+	 * 
+	 * <p>
+	 * 
+	 * 使用样例<br>
+	 * String
+	 * springXmlParentPath=ConfigUtil.getConfigPathFromDefault("config/applicationContext.xml","classpath:config/applicationContext.xml");<br>
+	 * ApplicationContext ctx = new
+	 * FileSystemXmlApplicationContext(springXmlParentPath);<br>
+	 * 
+	 * </p>
+	 * 
+	 * @param filePath
+	 *            文件相对路径 优先从 工作空间下找的路径
+	 * @param defaultPath
+	 *            默认路径 如果从工作空间下找不到则返回该默认路径
+	 * @param isOutputLog
+	 *            是否输出日志
+	 * @return String 路径
+	 */
+	public static String getConfigPathFromDefault(String filePath, String defaultPath, boolean isOutputLog) {
 		String workPath = System.getProperty("user.dir");
 		// System.out.println("[ConfigUtil]工作空间路径: " + workPath);
 		String confPath = workPath + File.separator + filePath;
 		File file = new File(confPath);
 		if (file.exists() && file.isFile()) {
-			System.err.println("[ConfigUtil]使用工作空间路径:" + confPath);
+			if (isOutputLog) {
+				System.err.println("[ConfigUtil]使用工作空间路径:" + confPath);
+			}
 			return confPath;
 		}
 
-		System.err.println("[ConfigUtil]使用默认路径:" + defaultPath);
+		if (isOutputLog) {
+			System.err.println("[ConfigUtil]使用默认路径:" + defaultPath);
+		}
 		// 如果找不到就用默认的
 		return defaultPath;
 	}
