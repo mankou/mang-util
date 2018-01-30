94dc8c4047026b56f94c782086ea1f339ecaf8e9
mang <ningma003@gmail.com>
2018-01-30 14:02:26 +0800

    * test subString

--- src/test/java/mang/util/StringUtilTest.java
+++ src/test/java/mang/util/StringUtilTest.java
@@ -3,6 +3,8 @@ package mang.util;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import junit.framework.AssertionFailedError;
 public class StringUtilTest {
 	
 	@Test
@@ -28,6 +30,20 @@ public class StringUtilTest {
 		System.out.println("=============");
 		System.out.println("原始字符串:"+str3);
 		System.out.println("处理后:"+result);
+	}
+	
+	@Test
+	public void testSubString(){
+		String str="abcdefg";
+		String result=StringUtils.substring(str, 0, 6);
+		System.out.println(result);
+		
+		String result2=StringUtils.substring(str, 0, 10);
+		System.out.println(result2);
+		
+		//下面报错
+		String result3=str.substring(0, 10);
+		System.out.println(result3);
 	}
 
 }
